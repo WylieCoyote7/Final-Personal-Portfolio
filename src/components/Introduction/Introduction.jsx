8f8f6b04230d0cd0d0ebf55a27701b1c38553066
WylieCoyote7 <92611672+WylieCoyote7@users.noreply.github.com>
2024-04-11 00:22:47 +0000

    Added intro and such

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -6,13 +6,14 @@ const ProfileSection = () => {
   return (
     <div className="profile-section">
       <img src="profilepic.jpg" alt="profile" clasName="pofilepic" />
-      <h1>Software engineer, father, and believer</h1>
+      <h1>Cybersecurity analyst, software enthusiast</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur
-        adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi..
+        My name is Alex Wylie and I am currently a cybersecurity analyst. I also love to learn about code and
+        how to build software and scripts that make life easier. Technology is supposed to be used, so
+        let's use it!
       </p>
       <div className="social-icons">
-        <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer"></a>
+        <a href="https://www.linkedin.com/in/alexander-wylie-8867a9157/" target="_blank" rel="noopener noreferrer"></a>
         <a href="https://www.xing.com/profile/yourprofile" target="_blank" rel="noopener noreferrer"></a>
         <a href="/share" target="_blank" rel="noopener noreferrer"></a>
       </div>
